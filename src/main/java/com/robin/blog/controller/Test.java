761378deb5dcd726d443e54f6fc394356d90201a
YangRobin <1585598295@qq.com>
2019-05-21 00:57:46 +0800

    set basic config

--- src/main/java/com/robin/blog/controller/Test.java
+++ src/main/java/com/robin/blog/controller/Test.java
@@ -6,8 +6,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Test {
 
+  // @Value("${robin.msg}")
+  // private String msg;
+
   @RequestMapping(value = "hello")
   public String helloworld() {
+    // System.out.println(msg);
     return "helloworld";
   }
 }
